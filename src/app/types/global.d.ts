0eef1ae14bfb71c3d346988e9bead9eb4fbbeea1
Denis Pereloma <den.pereloma@gmail.com>
2024-03-19 11:59:33 +0300

    feature/YH-34: fix type svg

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -10,8 +10,8 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg' {
-  import React from 'react';
+  import { FC, SVGProps } from 'react';
 
-  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+  const SVG: FC<SVGProps<SVGSVGElement>>;
   export default SVG;
 }
