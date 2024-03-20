add07074c9fd6d76cf7b3b73575981e438bfe7f9
PerelomaDenis <88330372+PerelomaDenis@users.noreply.github.com>
2024-03-20 22:30:45 +0300

    Feature/YH-34 added eslint, prettier, husky, lint staged, stylelint (#2)
    * feature/YH-34: add eslint, prettier, stylelint, husky, lint staged
    
    * feature/YH-34: add css modules plugin
    
    * feature/YH-34: update readme
    
    * feature/YH-34: add fsd plugin
    
    * feature/YH-34: fix pr comments
    
    * feature/YH-34: fix error
    
    * feature/YH-34: fix type svg
    
    * feature/YH-34: fix type svg

--- config/webpack/webpackConfig.ts
+++ config/webpack/webpackConfig.ts
@@ -1,24 +1,25 @@
 import { Configuration } from 'webpack';
+
+import { WebpackOptions } from './types/types';
 import { webpackDevServer } from './webpackDevServer';
 import { webpackLoaders } from './webpackLoaders';
+import { webpackOutput } from './webpackOutput';
 import { webpackPlugins } from './webpackPlugins';
 import { webpackResolvers } from './webpackResolvers';
-import { WebpackOptions } from './types/types';
-import { webpackOutput } from './webpackOutput';
 
 export const webpackConfig = (options: WebpackOptions): Configuration => {
-    const { isDev, mode, paths } = options;
+  const { isDev, mode, paths } = options;
 
-    return {
-        mode: mode ?? 'development',
-        entry: paths.entry,
-        module: {
-            rules: webpackLoaders(options),
-        },
-        resolve: webpackResolvers(options),
-        output: webpackOutput(options),
-        plugins: webpackPlugins(options),
-        devServer: isDev && webpackDevServer(options),
-        devtool: isDev ? 'inline-source-map' : 'source-map'
-    }
-}
+  return {
+    mode: mode ?? 'development',
+    entry: paths.entry,
+    module: {
+      rules: webpackLoaders(options),
+    },
+    resolve: webpackResolvers(options),
+    output: webpackOutput(options),
+    plugins: webpackPlugins(options),
+    devServer: isDev ? webpackDevServer(options) : undefined,
+    devtool: isDev ? 'inline-source-map' : 'source-map',
+  };
+};
