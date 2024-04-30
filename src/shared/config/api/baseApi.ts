6fbfca6489ddfbfa25f8b0d75a60a7f7e8fa91e7
Denis Pereloma <den.pereloma@gmail.com>
2024-04-30 16:53:08 +0300

    YH-58: add delete question

--- src/shared/config/api/baseApi.ts
+++ src/shared/config/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import { ApiTags } from '@/shared/config/api/apiTags';
+import { ApiTags } from './apiTags';
 
 export const baseApi = createApi({
 	tagTypes: Object.values(ApiTags),
@@ -8,10 +8,13 @@ export const baseApi = createApi({
 	baseQuery: fetchBaseQuery({
 		baseUrl: process.env.API_URL,
 		prepareHeaders: (headers) => {
-			headers.set(
-				'Authorization',
-				`Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJAZXhhbXBsZS5jb20iLCJzdWIiOiI3Zjg2YTU5ZC02OTcwLTQxZjAtOTk5Yy00YWQ4ZDQ1ZTMyNjYiLCJpYXQiOjE3MTQzOTU2NzYsImV4cCI6MTcxNDQ4MjA3Nn0.uLEcLKgBjBKBTxQ1HymenuvOAfwgsgkxwGH7tFwLPMo`,
-			);
+			const accessToken = localStorage.getItem('accessToken');
+
+			if (accessToken) {
+				headers.set('Authorization', `Bearer ${accessToken}`);
+			}
+
+			return headers;
 		},
 	}),
 	endpoints: () => ({}),
