4baec79d92ca9543a434589de681c9dd23fd4c4c
Fr1Day12 <106734778+Fr1Day12@users.noreply.github.com>
2024-10-20 12:40:48 +0300

    YH-438: fixed header and Popover arrangement of elements (#45)

--- src/entities/auth/ui/Avatar/Avatar.tsx
+++ src/entities/auth/ui/Avatar/Avatar.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
+import { Button } from 'yeahub-ui-kit';
 
 import { selectUserProfile } from '../../model/selectors/authSelectors';
 
 import styles from './Avatar.module.css';
 
-export const Avatar = () => {
+type AvatarProps = {
+	onClick?: () => void;
+};
+
+export const Avatar = ({ onClick }: AvatarProps) => {
 	const user = useSelector(selectUserProfile);
 	const { avatarUrl } = user ?? { avatarUrl: '' };
 
 	return (
-		<div className={styles.avatar}>
+		<Button theme="tertiary" className={styles.avatar} onClick={onClick}>
 			<div className={styles.imagewrap}>
 				{<img src={avatarUrl} className={styles.image} alt="avatar" />}
 			</div>
-		</div>
+		</Button>
 	);
 };
